050b0180666c6ada85b1c717011231d3d2476123
MaazAhmad <2400101530068@aktu.edu.in>
2026-03-10 07:29:07 +0000

    done

---
+++ @/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>maaz</h1>
+      <h1>ahmad</h1> 
+      <user></user>
+    </div>
+  )
+}
+function user()
+{
+  return(
+    <h1>statement</h1>
+  )
+
+}
+export default App
